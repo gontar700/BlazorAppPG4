9dd045b5dd306f7d8aa453f2e50f3df92ad54e92
danielgontar@gmail.com <danielgontar@gmail.com>
2024-11-26 08:51:33 +0200

    Add project files.

---
+++ BlazorAppPG4/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorAppPG4
+{
+    public class Person
+    {
+        private String firstName;
+
+        private String lastName;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+    }
+}
